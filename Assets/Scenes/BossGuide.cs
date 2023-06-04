0addce238a3ccd0fb44f8df75dcea443132e8fcf
Mangjik <red9915@naver.com>
2023-06-04 13:49:21 +0900

    스테이지 선택창 버그 수정
    .

--- Assets/Scenes/BossGuide.cs
+++ Assets/Scenes/BossGuide.cs
@@ -7,19 +7,22 @@ public class BossGuide : MonoBehaviour
     private bool state;
 
     public GameObject boss;
+
+
     // Start is called before the first frame update
     void Start()
     {
         state = false;
     }
+    public void bosse()
+    {
+        boss.SetActive(true);
+        state = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            boss.SetActive(true);
-            state = true;
-        }
+
     }
 }
