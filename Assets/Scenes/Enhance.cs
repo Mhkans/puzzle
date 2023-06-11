a81c770a0361fde465bb2a22656312ea4889b700
minsoo4781 <49774256+minsoo4781@users.noreply.github.com>
2023-06-11 18:43:23 +0900

    최종버전 제출용

--- Assets/Scenes/Enhance.cs
+++ Assets/Scenes/Enhance.cs
@@ -19,8 +19,8 @@ public class Enhance : MonoBehaviour
                            "적의 공격력 : " + 5 +"\n\n" +*/
                            "    PLAYER  " + " \n " +
                            "\n현재 공격력 : 블럭당 " + 2 * Player.attCoefficient  +
+                           "\n현재 회복력 : 블럭당" + 2 * Player.healCoefficient +
                            "\n속성 공격력 : 3블럭일때 " + 2 * Player.attCoefficient*8  +
-                           "\n현재 회복력 : 핑크블럭당" + 2 * Player.healCoefficient +
                            "\n\n속성 블럭을 길게 연결할수록 \n더 강해집니다\n" +
                            "\n현재 방어력 : "  + Player.shield+ " "+
                            "\n현재 체력 : " + 100 * Player.hpCoefficient +
